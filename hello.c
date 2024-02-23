870c6584ee5f18386317b6c9eb61f65840fd4740
Sahil <sahilbansal5307@gmail.com>
2024-02-22 22:10:44 -0800

    base of calc

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int a,b;
+char ch;
+printf("enter the first no. ");
+scanf("%d",&a);
+printf("enter the second no. ");
+scanf("%d",&b);
+printf("enter the operater ");
+scanf("%c", &ch);
+return 0;
+}
